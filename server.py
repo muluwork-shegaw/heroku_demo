8c74bd7ffc5e7b938bb69f83ee995879443aefa2
Muluwork <37736254+muluwork-shegaw@users.noreply.github.com>
2020-08-19 22:15:20 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,32 @@
+import numpy as np
+from flask import Flask, request, jsonify,render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    for rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0],2)
+    return render_template('index.html',
+     prediction_text = 'salary is {}'.format(output) )
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+# @app.route('/about',methods=['GET'])
+# def about():
+#     return "<h1> About maggie</h1>"
+# if __name__ == '__main__':
+#     app.run(host = 'localhost',port=3333,debug = True)
